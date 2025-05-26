25c0b7fa547b5493049e20fd92c68a99c8dc5e51
stevi-takang-3il <takangas@3il.fr>
2025-05-26 18:02:50 +0100

    changement webpublic

--- src/main/java/boldair/web/WebPublic.java
+++ src/main/java/boldair/web/WebPublic.java
@@ -41,22 +41,20 @@ public class WebPublic {
 	public String quiSommesNous() {
 		return "public/qui-sommes-nous";
 	}
-	/*
-	 * <<<<<<< HEAD
-	 *
-	 * @GetMapping( "/inscription" ) public String inscription() { return
-	 * "public/inscription"; }
-	 *
-	 * @GetMapping( "/benevole" ) public String bénévole() { return
-	 * "public/benevole"; }
-	 *
-	 * @GetMapping( "/dashbord" ) public String dashbord() { return
-	 * "public/dashbord"; } =======
-	 *
-	 * @GetMapping("/all-event") public String getAllEvent(Model model) {
-	 * List<Evenement> items = daoEvenement.findEvenementsActifs();
-	 * model.addAttribute( "list", items ); return "/all-evenements"; } >>>>>>>
-	 * 7f1b5eae293722107b14f83637c2cda6705c803f
-	 */
-
+	
+	
+	 
+	  @GetMapping( "/inscription" ) public String inscription() { return
+	  "public/inscription"; }
+	 
+	  @GetMapping( "/benevole" ) public String bénévole() { return
+	  "public/benevole"; }
+	 
+	  @GetMapping( "/dashbord" ) public String dashbord() { return
+	  "public/dashbord"; } 
+	 
+	  @GetMapping("/all-event") public String getAllEvent(Model model) {
+	  List<Evenement> items = daoEvenement.findEvenementsActifs();
+	  model.addAttribute( "list", items ); return "/all-evenements"; } 
+	
 }
