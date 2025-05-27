dff146d81f0e06cb9e2e1ee94325ee46e03e2bb3
KOTTE TCHOUBANG Shekinah <kottetcs@3il.fr>
2025-05-27 14:09:11 +0200

    Reglement des conflits de stevi

--- src/main/java/boldair/web/WebPublic.java
+++ src/main/java/boldair/web/WebPublic.java
@@ -41,20 +41,29 @@ public class WebPublic {
 	public String quiSommesNous() {
 		return "public/qui-sommes-nous";
 	}
-	
-	
-	 
-	  @GetMapping( "/inscription" ) public String inscription() { return
-	  "public/inscription"; }
-	 
-	  @GetMapping( "/benevole" ) public String bénévole() { return
-	  "public/benevole"; }
-	 
-	  @GetMapping( "/dashbord" ) public String dashbord() { return
-	  "public/dashbord"; } 
-	 
-	  @GetMapping("/all-event") public String getAllEvent(Model model) {
-	  List<Evenement> items = daoEvenement.findEvenementsActifs();
-	  model.addAttribute( "list", items ); return "/all-evenements"; } 
-	
+
+	@GetMapping( "/inscription" )
+	public String inscription() {
+		return "public/inscription";
+	}
+
+	@GetMapping( "/benevole" )
+	public String bénévole() {
+		return "public/benevole";
+	}
+
+	@GetMapping( "/dashbord" )
+	public String dashbord() {
+		return "public/dashbord";
+	}
+
+	@GetMapping( "/all-event" )
+	public String getAllEvent( Model model ) {
+		List<Evenement> items = daoEvenement.findEvenementsActifs();
+		model.addAttribute( "list", items );
+		return "/all-evenements";
+	}
+
+	// hello
+
 }
