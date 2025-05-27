3c83402a259ffae33348fdecd468c2a028fc9c1a
Mohamed AYEVA <mohamed.ayeva.123@gmail.com>
2025-05-27 15:43:31 +0200

    Le projet à jour qui marche

--- src/main/java/boldair/web/WebParticipant.java
+++ src/main/java/boldair/web/WebParticipant.java
@@ -2,14 +2,11 @@ package boldair.web;
 
 import java.io.IOException;
 import java.sql.Date;
-import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
